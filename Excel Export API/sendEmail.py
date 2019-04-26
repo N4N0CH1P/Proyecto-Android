f8688aad656300d8ed530a3b19a8f94e10b7bc90
hugoyevides <victor.covarrubias@hotmail.com>
2019-04-26 15:32:50 -0500

    Final

--- Excel Export API/sendEmail.py
+++ Excel Export API/sendEmail.py
@@ -2,10 +2,13 @@ import smtplib
 import mysql_config as mysql
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
+from email.MIMEBase import MIMEBase
+from email import Encoders
+import os
 
 #Declaracion de informacion del servidor
-emailServerAddressSMTP="smtp.gmail.com"
-SMTPPort=587
+emailServerAddressSMTP="mail.orbi.mx"
+SMTPPort=25
 smtpUsername="msva@orbi.mx"
 smtpPassword="msvaemail"
 
@@ -20,30 +23,48 @@ server.login(smtpUsername, smtpPassword)
 
 #Funcion para obtener la direccion de correo electronico de un userID
 def getUserEmailAddress(userID):
+    print("Conectando con base de datos para obtener email destinatario...")
     #Conseguir el correo del usuario
     data=mysql.fetchDataFromDatabase("SELECT email FROM usuario WHERE userID='"+userID+"'")
     #Ver si tenemos datos
     if data:
         row=mysql.getFirstElement(data)
-        if "email" in row:
-            return row["email"]
-        else:
-            return None
+        print("Success!!")
+        return row[0]
     else:
+        print("ERROR!!!")
         return None
 
 def sendEmailToUser(userIDData,userIDDest):
     #Conseguir el correo del usuario para obtener la direccion
     mail=getUserEmailAddress(userIDDest)
     if(mail):
+        print("Preparando correo electronico...")
         #preparar el correo
         msg = MIMEMultipart()
         msg['From'] = smtpUsername
         msg['To'] = mail
-        msg['Subject'] = "Python email"
-        body = "Python test mail"
+        msg['Subject'] = "Historial tomas de presion para el usuario " + userIDData
+        body = "Se anexa el archivo excel con el historial del usuario " + userIDData
         msg.attach(MIMEText(body, 'plain'))
+        print("Success!!")
+        print("Agregando archivo "+userIDData+".xlsx a correo electronico")
+        #Agregamos el archivo
+        part = MIMEBase('application', "octet-stream")
+        part.set_payload(open(userIDData+".xlsx", "rb").read())
+        Encoders.encode_base64(part)
+        part.add_header('Content-Disposition', 'attachment; filename="'+userIDData+'.xlsx"')
+        msg.attach(part)
         text = msg.as_string()
+        print("Success!!")
+        print("Enviando email...")
         server.sendmail(smtpUsername, mail, text)
+        print("Correo enviado con exito a "+mail)
+        #Eliminar el arhcivo
+        print("Eliminando archivo cache")
+        os.remove(userIDData+".xlsx")
+        print("Success!!")
+        resultJson={"success":"yes"}
+        return resultJson
     else:
         return mysql.sendErrorMssg("Error consiguiendo el correo electronico del usuario")
--- Excel Export API/validateUser.py
+++ Excel Export API/validateUser.py
@@ -1,24 +1,16 @@
-import MySQLdb
-import mysql_config
-from excelapi import sendErrorMssg
+import mysql_config as mysql
 
 #Funcion para validar usuario
 def validateUser(userID, userPassword):
-    #prepara query
-    query="SELECT password FROM usuario WHERE userID='"+userID+"'"
-    #Iniciamos el cursor dentro de nuestra base de datos
-    cursor=db.cursor()
-    #hacer query
-    try:
-        cursor.execute(query)
-        row=cursor.fetchone()
-        #Ver si las claves estan correctas
-        if(row["password"]!=userPassword):
-            print("Las claves no son validas para el usuario " + userID)
-            return sendErrorMssg("Error, las claves no son correctas")
-        #Regresar success
-        returnJson={"success","yes"}
-        return returnJson
-    except:
-        print("Error verificando usuario y contrasenia, problemas con conexion a base de datos")
-        return sendErrorMssg("Error haciendo query a la base de datos")
+    print("Validando usuario con la base de datos....")
+    #Pedir al servidor mysql los datos
+    data=mysql.fetchDataFromDatabase("SELECT password FROM usuario WHERE userID='"+userID+"'")
+    row=mysql.getFirstElement(data)
+    #Ver si las claves estan correctas
+    if(row[0]!=userPassword):
+        print("Las claves no son validas para el usuario " + userID)
+        return mysql.sendErrorMssg("Error, las claves no son correctas")
+    #Regresar success
+    print("Usuario validado con exito!")
+    returnJson={"success","yes"}
+    return returnJson
