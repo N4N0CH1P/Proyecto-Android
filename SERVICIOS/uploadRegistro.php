aee147159d3ad4aad90bf7e71229e2da32a84762
hugoyevides <victor.covarrubias@hotmail.com>
2019-05-06 12:34:18 -0500

    Fixed Date Problem

--- SERVICIOS/uploadRegistro.php
+++ SERVICIOS/uploadRegistro.php
@@ -13,8 +13,9 @@
             die();
         }
     }
+    $fecha= date('Y-m-d');
     //Creamos un nuevo objeto de la clase presion para meter todos los datos
-    $newPresion=new Presion($_POST["presionID"],$_POST["presionAsist"],$_POST["presionDist"],$_POST["presionAsistMan"],$_POST["presionDistMan"],getdate());
+    $newPresion=new Presion($_POST["presionID"],$_POST["presionAsist"],$_POST["presionDist"],$_POST["presionAsistMan"],$_POST["presionDistMan"],$fecha);
     //Creamos la instancia del usuario
     $myUser=new Usuario(null,null,null,null,null,null,null);
     $myUser->populateDataFromDatabase($_POST["userID"]);
